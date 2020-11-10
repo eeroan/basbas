5bc54a6fd1ba8d6b83eb6a85388bb708cb9cbde7
Jani Luukko <jani.luukko@reaktor.com>
2020-11-10 19:38:10 +0200

    Add BasBas kulma reservations

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000
 const startMsg = '\033[33mServer started in \033[36mhttp://localhost:' + port + ', \033[33mtook \033[39m'
 const startedTime = new Date().toString()
 const baseUrl = 'https://v2.tableonline.fi/instabook/bookings'
-const urlForDate = persons => `${baseUrl}/availabilities/t7c5hxB/589/${persons}?locale=fi&date=`
+const kulmaId = ['kxSrGpi', '1100']
+const bistroId = ['t7c5hxB', '589']
+const urlForDate = (persons, kulma) => `${baseUrl}/availabilities/${kulma ? kulmaId[0] : bistroId[0]}/${kulma ? kulmaId[1] : bistroId[1]}/${persons}?locale=fi&date=`
 console.time(startMsg)
 http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true)
@@ -16,7 +18,8 @@ http.createServer((req, res) => {
   const isGet = req.method === 'GET'
   const persons = parsedUrl.query && parsedUrl.query.persons ? Number(parsedUrl.query.persons) : 2
   if (isGet && uri === '/' && persons >= 1) {
-    writePage(res, persons)
+    const kulma = parsedUrl.query && parsedUrl.query.kulma
+    writePage(res, persons, kulma)
   } else if (isGet && uri.startsWith('/public'))
     serveStatic(uri, res)
   else
@@ -33,12 +36,12 @@ const serveStatic = (uri, res) => {
     res.end()
   })
 }
-const writePage = async (res, persons) => {
+const writePage = async (res, persons, kulma) => {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
   res.write('<!DOCTYPE html>')
-  res.write(head)
+  res.write(head(kulma))
   const dates = [...Array(33).keys()].map(delta => dateUtil.addDay(dateUtil.toMidnight(new Date()), delta)).filter(dateUtil.isOpen)
-  const dayAvailabilities = await combineArray(dates, persons)
+  const dayAvailabilities = await combineArray(dates, persons, kulma)
   res.end(
     dayAvailabilities.map(({date, body}) => {
       const markup = body.match(/\$\("#availabilities"\)\.html\('([\s\S]*)'\);/)[1]
@@ -55,12 +58,12 @@ const notFound = res => {
   res.writeHead(404)
   res.end()
 }
-const dateToUrl = (date, persons) => {
-  return `${urlForDate(persons)}${dateUtil.formatReverseIsoDate(date)}`
+const dateToUrl = (date, persons, kulma) => {
+  return `${urlForDate(persons, kulma)}${dateUtil.formatReverseIsoDate(date)}`
 }
-const combineArray = async (dates, persons) => Promise.all(dates.map(date => dateAndBody(date, persons)))
-const dateAndBody = async (date, persons) => {
-  const body = await getCached(dateToUrl(date, persons))
+const combineArray = async (dates, persons, kulma) => Promise.all(dates.map(date => dateAndBody(date, persons, kulma)))
+const dateAndBody = async (date, persons, kulma) => {
+  const body = await getCached(dateToUrl(date, persons, kulma))
   return {date, body}
 }
 const responses = {}
@@ -93,7 +96,7 @@ const get = async (uri) => new Promise(resolve => {
   else http.get(options, cbFunc)
 })
 
-const head = `<html>
+const head = kulma => `<html>
 <head>
 <!-- Server started: ${startedTime}-->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4154602-10"></script>
@@ -103,7 +106,7 @@ const head = `<html>
   gtag('js', new Date());
   gtag('config', 'UA-4154602-10');
 </script>
-<title>Basbas</title>
+<title>BasBas</title>
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link rel="apple-touch-icon" href="menu.png"/>
@@ -113,7 +116,8 @@ const head = `<html>
 <body>
 <article>
 <header>
-<h1>Basbas pöytä&shy;varaukset</h1>
+<h1>${kulma ? 'BasBas Kulma' : 'BasBas'} pöytä&shy;varaukset</h1>
 <p class="subtitle"><a href="http://basbas.fi/bistro#reservation" target="_blank">Varaa täältä</a></p>
+${kulma ? '<p class="subtitle"><a href="/">BasBas Bistro pöytä&shy;varaukset</a></p>' : '<p class="subtitle"><a href="/?kulma=1">BasBas kulma pöytä&shy;varaukset</a></p>'}
 </header>
 </article>`
